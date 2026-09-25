f694893f84c71c21265d3952e07daad7fb0c56bd
Abhijith <55525183+Abhijith792001@users.noreply.github.com>
2026-09-25 07:26:54 +0000

    practice if else conditions

---
+++ Day 1/check_number.py
@@ -0,0 +1,20 @@
+numbers = [10, 25, 30, 45, 50]
+
+# def chec_number():
+#         greater = False
+#         for x in numbers:
+#             if x < 30:
+#                 greater = True
+#                 break
+#         if greater:
+#           print("Greater");
+#         else:
+#          print("30 or less");
+
+# print(chec_number);
+
+for x in numbers:
+    if x > 30:
+        print("Greater");
+    else:
+        print("30 or less")    
